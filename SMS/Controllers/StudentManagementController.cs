f937ec6db60a856509906667a1aac3e974784031
Pm1s <muhammadnazreen.asri@student.gmi.edu.my>
2024-06-06 17:44:46 +0800

    Add project files.

---
+++ SMS/Controllers/StudentManagementController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using SMS.Models;
+using System.Collections;
+using StudentManagementSystem.Models;
+
+namespace SMS.Controllers
+{
+    public class StudentManagementController : Controller
+    {
+        private readonly IConfiguration configuration;
+        public StudentManagementController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+        IList<StudentManagement> GetDbList()
+        {
+            IList<StudentManagement> dbList = new List<StudentManagement>();
+            SqlConnection conn = new SqlConnection(configuration.GetConnectionString("ParcelConnStr"));
+
+            string sql = @"SELECT * FROM course";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    dbList.Add(new StudentManagement()
+                    {
+                        
+                        courseName = reader.GetString(1),
+                        
+                    });
+                }
+            }
+
+            catch
+            {
+                RedirectToAction("Error");
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+            return dbList;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult addStudent(StudentManagement add)
+        {
+            SqlConnection conn = new SqlConnection(configuration.GetConnectionString("ParcelConnStr"));
+            SqlCommand cmd = new SqlCommand("spInsertStudent", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            // Add parameters
+            cmd.Parameters.AddWithValue("@name", add.Name);
+            cmd.Parameters.AddWithValue("@ic", add.IC);
+            cmd.Parameters.AddWithValue("@address", add.Address);
+            cmd.Parameters.AddWithValue("@intake", add.Intake);
+            cmd.Parameters.AddWithValue("@age", add.Age);
+            cmd.Parameters.AddWithValue("@phone", add.Phone);
+            cmd.Parameters.AddWithValue("@personalemail", add.PersonalEmail);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception message
+                Console.WriteLine("Error: " + ex.Message);
+                return View(add);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View("addStudent", add);
+        }
+
+
+        public IActionResult addCourse(StudentManagement course)
+        {
+            SqlConnection conn = new SqlConnection(configuration.GetConnectionString("ParcelConnStr"));
+            SqlCommand cmd = new SqlCommand("spInsertCourse", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            //sender
+
+            cmd.Parameters.AddWithValue("@coursedepartment", course.IndexDept);
+            cmd.Parameters.AddWithValue("@coursename", course.courseName);
+           
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            catch
+            {
+                return View(course);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+
+
+            return View("addCourse", course);
+        }
+
+        public IActionResult error()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ SMS/Models/StudentManagementSystem.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class StudentManagement
+    {
+        [Display(Name = "Student Id")]
+        public string StudentId
+        {
+            get
+            {
+                string hexTicks = DateTime.Now.Ticks.ToString("X");
+                return hexTicks.Substring(hexTicks.Length - 15, 9);
+            }
+
+            set { }
+        }
+
+        // add student
+        [Required]
+        [Display(Name = "Student Id")]
+        public string ViewId { get; set; }
+
+        [Required]
+        [Display(Name = "Student Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Ic")]
+        public string IC { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Age")]
+        public string Age { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+
+        [Required]
+        [Display(Name = "Personal Email")]
+        public string PersonalEmail { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string studentEmail { get; set; }
+
+        [Required]
+        [Display(Name = "Course Name")]
+        public string Course { get; set; }
+
+
+
+        [Display(Name = "Course Name")]
+        public string courseName { get; set; }
+
+        // Dict department
+
+        [Display(Name = "Course Department")]
+        public int IndexDept { get; set; }
+        public int courseDept { get; set; }
+        public IDictionary<int, string> DictDept
+        {
+            get
+            {
+                return new Dictionary<int, string>()
+                {
+                    {1, "Electrical Engineering" },
+                    {2, "Mechanical Engineering" },
+                    {3, "Computer And Information" }
+
+                };
+            }
+
+            set { }
+        }
+
+        // Dict Intake
+        [Required]
+        [Display(Name = "Intake")]
+        public int Intake { get; set; }
+
+        [Display(Name = "Intake")]
+        public IDictionary<int, string> DictIntake
+        {
+            get
+            {
+                return new Dictionary<int, string>()
+                {
+                   
+                    {1, "2013" },
+                    {2, "2014" },
+                    {3, "2015" },
+                    {4, "2016" },
+                    {5, "2017" },
+                    {6, "2018" },
+                    {7, "2019" },
+                    {8, "2020" },
+                    {9, "2021" },
+                    {10, "2022" },
+                    {11, "2023" },
+                    {12, "2024" }
+                };
+            }
+            set { }
+        }
+
+        //Dict course
+        [Required]
+        [Display(Name = "Course")]
+        public string studentCourse { get; set; }
+
+        [Display(Name = "Course")]
+        public IDictionary<int, string> DictCourse
+        {
+            get
+            {
+                return new Dictionary<int, string>()
+                {
+                    {0, "" },
+                    {1, "BSE" },
+                    {2, "NVM" },  //dummy data
+                    {3, "IDK" },
+                    {4, "TRY" },
+                    {5, "HARD" }
+                };
+            }
+            set { }
+        }
+
+        
+
+    }
+    
+}
